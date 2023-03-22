aed69f3248bce2fdd6443aed9eaaab0ff1e46016
VictorLuizS <victor@fabrica.com.br>
2023-03-22 16:56:32 -0300

    home faq

--- front-page.php
+++ front-page.php
@@ -8,6 +8,7 @@
     <?php get_template_part('./template-parts/home/especialidades'); ?>
     <?php get_template_part('./template-parts/home/estrutura'); ?>
     <?php get_template_part('./template-parts/home/social-proof'); ?>
+    <?php get_template_part('./template-parts/home/faq'); ?>
 
 </main>
 
