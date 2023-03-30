b9e5e1339a457e40bbce22e5bf61906e9448bdcc
VictorLuizS <victor@fabrica.com.br>
2023-03-30 10:49:10 -0300

    burgermenu alterado para ser funcional apenas com CSS

--- scripts/header.js
+++ scripts/header.js
@@ -1,19 +1,4 @@
-// MOBILE MENU
-
-var isMobile = window.matchMedia("only screen and (max-width: 1023px)").matches;
-
-if(isMobile){
-    var $menu = document.querySelector('.header__nav');
-    var $open_button = document.querySelector('.header__open-menu-button');
-    
-    $menu.classList.add('header__nav--hidden');
-    
-    $open_button.addEventListener('click', toggleMenuVisibility);
-    
-    function toggleMenuVisibility(){
-        $menu.classList.toggle('header__nav--hidden');
-    }
-}
+// SCROLLING EVENTS
 
 var $header_bar = document.querySelector('.header');
 var $fake_header_bar = document.querySelector('.fake-header');
