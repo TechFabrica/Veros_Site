2970b31e9c6f89f30a18ccce08372bde3851581f
VictorLuizS <victor@fabrica.com.br>
2023-05-04 17:33:26 -0300

    novo layout da página de PS e Ambulância

--- ps-ambulancia.php
+++ ps-ambulancia.php
@@ -10,6 +10,8 @@ Template Name: PS e Ambulância
     
     <?php get_template_part('./template-parts/ps-ambulancia/hero'); ?>
     <?php get_template_part('./template-parts/ps-ambulancia/main'); ?>
+    <?php get_template_part('./template-parts/ps-ambulancia/triagem'); ?>
+    <?php get_template_part('./template-parts/ps-ambulancia/main-2'); ?>
     <?php get_template_part('./template-parts/common/como-funciona'); ?>
     <?php get_template_part('./template-parts/common/faq'); ?>
     <?php get_template_part('./template-parts/common/contact'); ?>
